63af413dfbc8303696e809dd030859f361569ba6
Eamonn de Leastar <edeleastar@wit.ie>
2023-02-09 09:25:42 +0000

    visual components

---
+++ src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Icon from 'svelte-awesome';
+	import { faMap } from '@fortawesome/free-regular-svg-icons';
+	import { faMapMarked } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="columns is-vcentered">
+	<div class="column is-two-thirds">
+		<div class="box has-text-centered columns m-2">
+			<div class="column">
+				<img src="/favicon.png" width="60" alt="img" />
+			</div>
+			<div class="column">
+				<div class="title is-5">Oileain</div>
+				<div class="subtitle is-5">islands of Ireand</div>
+			</div>
+			<div class="column">
+				<i class="fas fa-donate fa-3x" style="color:rgb(95, 96, 173)" />
+			</div>
+		</div>
+	</div>
+	<div class="column">
+		<div class="box has-text-centered columns m-2 is-2">
+			<a href="/" class="column">
+				<Icon data={faMap} scale={4} />
+			</a>
+			<a href="/navigator" class="column">
+				<Icon data={faMapMarked} scale={4} />
+			</a>
+		</div>
+	</div>
+</div>
---
+++ src/lib/IslandDescription.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Island } from '../services/oileain-types';
+
+	export let island: Island;
+</script>
+
+<section class="section">
+	<div class="description">
+		{@html island.description}
+	</div>
+</section>
