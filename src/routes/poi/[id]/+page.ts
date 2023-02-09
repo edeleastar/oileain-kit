29ece98f192c1cf15e311b01109d2c3322b21ca6
Eamonn de Leastar <edeleastar@wit.ie>
2023-02-09 09:26:15 +0000

    poi page

---
+++ src/routes/poi/[id]/+page.ts
@@ -0,0 +1,13 @@
+import { generateMarkerSpec, oileain } from "../../../services/oileain-api";
+import type { PageLoad } from "./$types";
+
+export const ssr = false;
+
+export const load: PageLoad = async ({ params }) => {
+	const island = await oileain.getIslandById(encodeURI(params.id));
+	const marker = generateMarkerSpec(island);
+	return {
+		island,
+		marker
+	};
+};
